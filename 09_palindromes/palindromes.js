a19899d1d5c7d970eb57b1d0dcd63c4abebf8574
benny <benjaminbowden@gmail.com>
2023-09-21 16:56:44 +0400

    Complete the palidromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+function removeNonAlphaCharacters(str) {
+    return str.replace(/[^a-z]/gi, '');
+}
 
+const palindromes = function (str) {
+    let cleanStr = removeNonAlphaCharacters(str);
+    cleanStr = cleanStr.toLowerCase();
+
+    return cleanStr.split("").reverse().join("") === cleanStr;
 };
 
 // Do not edit below this line
