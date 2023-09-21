0308dae68b4f87ed6e4b226a5ea6184d702965a0
benny <benjaminbowden@gmail.com>
2023-09-21 14:26:24 +0400

    pass tests on all functions excluding factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,21 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b ) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	return myArray.reduce((total, item) => total + item, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+    return myArray.reduce((total, item) => total * item, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b ) {
+	return Math.pow(a, b);
 };
 
 const factorial = function() {
