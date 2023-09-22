06730def8aad4135bddc595607a2c91166e2257a
benny <benjaminbowden@gmail.com>
2023-09-22 18:04:48 +0400

    Find the oldest person from an array of people

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
 
+        if (Object.keys(oldest).length !== 0) {
+            if (!person.hasOwnProperty('yearOfDeath')) {
+                person.yearOfDeath = new Date().getFullYear();
+            }
+            if (!oldest.hasOwnProperty('yearOfDeath')) {
+                oldest.yearOfDeath = new Date().getFullYear(); // Assign current year only if yearOfDeath is missing
+            }
+            console.table(person)
+            return oldest.yearOfDeath - oldest.yearOfBirth > person.yearOfDeath - person.yearOfBirth ? oldest : person
+        }
+        else {
+            return person
+        }
+    }, {});
 };
 
 // Do not edit below this line
